42bffbc2ce665e72993d47ac18b905fc5b52f1d4
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-19 15:49:46 -0400

    Upgrade to ruby 3.3.5 and rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,11 @@ Bundler.require(*Rails.groups)
 
 module LoginRequiredLab
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in this directory are automatically loaded.
   end
 end
